1887483dc39493a66c1e61da7f0afebaf8013bc5
Kit Liu <Kit@KIT-LIU.local>
2017-01-21 04:26:54 -0800

    Added a button that increase money by 1

---
+++ Assets/MoneyUpdater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyUpdater : MonoBehaviour {
+	public int Money = 0;
+	public int Reward = 0;
+	public Text MoneyText;
+	public void increment(){
+		Money++;
+	}
+	//Money = increment(Money);
+		
+
+	// Use this for initialization
+	void Start () {
+		MoneyText = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Money = Money + Reward;
+		MoneyText.text = "Money: " + Money;
+	}
+}
