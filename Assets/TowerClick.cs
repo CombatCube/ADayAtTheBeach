023acb996173ea91b93239f28f8d050ab1a64643
Kit Liu <Kit@KIT-LIU.local>
2017-01-20 23:15:30 -0800

    gameobject changes colour when clicked

---
+++ Assets/TowerClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerClick : MonoBehaviour {
+	bool isClicked = false;
+
+	void OnMouseDown() {
+		isClicked = !isClicked;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isClicked) {
+			gameObject.GetComponent<SpriteRenderer> ().color = Color.black;
+		} else {
+			gameObject.GetComponent<SpriteRenderer> ().color = Color.cyan;
+		}
+	}
+}
