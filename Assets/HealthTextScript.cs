56a63466bdf061f6384216380ec9738d1f0c23d8
Niko <niko@savas.ca>
2017-01-21 03:01:29 -0800

    Made the basic Tower with text showing it's current health

---
+++ Assets/HealthTextScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthTextScript : MonoBehaviour {
+
+	private Canvas mainCanvas;
+	private GameObject healthTextObject;
+
+	// Use this for initialization
+	void Start () {
+		//Create a new UI object in the canvas
+		mainCanvas = GameObject.Find ("Canvas").GetComponent<Canvas> ();
+		healthTextObject = new GameObject ();
+		healthTextObject.transform.SetParent (mainCanvas.transform);
+
+		//Add text component
+		Text healthText = healthTextObject.AddComponent<Text> ();
+		Font ArialFont = (Font)Resources.GetBuiltinResource (typeof(Font), "Arial.ttf");
+		healthText.font = ArialFont;
+		healthText.material = ArialFont.material;
+		healthText.alignment = TextAnchor.UpperCenter;
+		healthText.fontSize = 18;
+
+		//Update text
+		updateHealth();
+
+		//Set position
+		healthText.transform.position = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthTextObject.GetComponent<Text>().transform.position = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+		updateHealth ();
+	}
+
+	private void updateHealth() {
+		Tower t = gameObject.GetComponent<Tower> ();
+		Text healthText = healthTextObject.GetComponent<Text> ();
+
+		healthText.text = t.currentHealth + "/" + t.totalHealth;
+	}
+}
