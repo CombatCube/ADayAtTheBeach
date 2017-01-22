de113f3df95ab76d323586abc93b41a77eb36adb
CombatCube <combatcu8e@gmail.com>
2017-01-22 14:16:54 -0800

    Added front wave, asset for back wave

--- Assets/WaveGenerator.cs
+++ Assets/WaveGenerator.cs
@@ -8,15 +8,20 @@ public class WaveGenerator : MonoBehaviour {
 	public GameObject wave;
 	public float worldHeight = 10;
 	public float worldWidth = 20;
+    public AudioClip clip;
 
-	private float timeSinceLastWave = 5;
-	
+    private float timeSinceLastWave = 5;
+
+    void Start()
+    {
+    }
+    
 	// Update is called once per frame
 	void Update () {
 		timeSinceLastWave += Time.deltaTime;
-
 		if (timeSinceLastWave >= timeBetweenWaves) {
 			Debug.Log ("Spawning Wave");
+
 			timeSinceLastWave = 0;
 
 			if (timeBetweenWaves > 5) {
@@ -24,8 +29,9 @@ public class WaveGenerator : MonoBehaviour {
 			}
 			GameObject newWave = Instantiate (wave);
 
-			//Super hacky don't do this at home kids
-			float xPosition = Random.Range (-1 * worldWidth, worldWidth);
+            newWave.GetComponent<AudioSource>().PlayOneShot(clip);
+            //Super hacky don't do this at home kids
+            float xPosition = Random.Range (-1 * worldWidth, worldWidth);
 
 			if (xPosition > 0) {
 				wave.transform.position = new Vector3 (xPosition - worldWidth/2, worldHeight/2f, -5f) * 1.5f;
