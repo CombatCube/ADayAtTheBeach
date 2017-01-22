a4fa2c590bfe2f877c42534cda83d81f884aa2fa
Kit Liu <Kit@KIT-LIU.local>
2017-01-21 16:28:36 -0800

    Big update, adding new console and money fields, working with fonts and buttons.

---
+++ Assets/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public GameObject wave;
+	// Use this for initialization
+	void Start () {
+		SpawnWave ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SpawnWave () {
+		int timeuntilwave;
+		int power;
+		GameObject newwave = Instantiate (wave);
+		newwave.transform.position = Random.insideUnitCircle;
+		newwave.transform.position += newwave.transform.position.normalized * 2f;
+		newwave.transform.LookAt(Vector3.zero);
+
+	}
+}
--- Assets/MoneyManager.cs
+++ Assets/MoneyManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoneyManager : MonoBehaviour {
 	public int Money;
 	public int Reward;
-	public int SandCost = 5;
-	public int TowerCost = 10;
+	public int SandCost;
+	public int TowerCost;
+	public ConsoleScript ConsoleText;
 	// Use this for initialization
 	void Start () {
 	
@@ -20,10 +22,14 @@ public class MoneyManager : MonoBehaviour {
 	 public void BuyTower(){
 		if ((Money - TowerCost) >= 0){
 			Money = Money - TowerCost;
+			ConsoleText.GetComponent<Text> ().text = "You've bought a tower!";
 			print("You've bought a tower!");
+
 		}
 		else {
+			
 			print("You don't have enough money!");
+			ConsoleText.GetComponent<Text> ().text = "You don't have enough money!";
 		}
 	}
 
@@ -31,9 +37,11 @@ public class MoneyManager : MonoBehaviour {
 		if ((Money - SandCost) >= 0){
 			Money = Money - SandCost;
 			print("You've bought sand!");
+			ConsoleText.GetComponent<Text> ().text = "You've bought sand!";
 		}
 		else {
 			print("You don't have enough money!");
+			ConsoleText.GetComponent<Text> ().text = "You don't have enough money!";
 		}
 	}
 }
