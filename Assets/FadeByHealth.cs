bd5e70578cdfdf7a948490726a523d688afa108a
Niko <niko@savas.ca>
2017-01-22 14:10:20 -0800

    Updated sand to be not a pink suh box

---
+++ Assets/FadeByHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeByHealth : MonoBehaviour {
+
+	public Health health;
+	public SpriteRenderer spriteRenderer;
+	
+	// Update is called once per frame
+	void Update () {
+		float healthPercent = health.currentHealth / health.maxHealth;
+
+		Color color = spriteRenderer.color;
+		color.a = healthPercent;
+		spriteRenderer.color = color;
+	}
+}
