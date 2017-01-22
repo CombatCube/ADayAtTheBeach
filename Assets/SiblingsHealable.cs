88be058a62f796e9954a102dc996190bd1e1c55a
Niko <niko@savas.ca>
2017-01-21 19:36:37 -0800

    Can now heal all sibling moats

---
+++ Assets/SiblingsHealable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SiblingsHealable : MonoBehaviour {
+
+	public float healthPerFrame;
+	public Health health;
+
+	void OnMouseOver() {
+		if (Input.GetMouseButton (0)) {
+			//Heal myself
+			if (health.currentHealth <= health.maxHealth) {
+				health.currentHealth += healthPerFrame;
+			}
+
+			//Heal my siblings
+			Health[] siblings = gameObject.transform.parent.gameObject.GetComponentsInChildren<Health>();
+
+			for (int i = 0; i < siblings.Length; i++) {
+				if (siblings [i].currentHealth <= siblings [i].maxHealth) {
+					siblings [i].currentHealth += healthPerFrame * 0.5f;
+				}
+			}
+		}
+	}
+}
