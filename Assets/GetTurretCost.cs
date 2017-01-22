b409ba3a0e705fc0176421b43f6f4f0ac2386f58
Kit Liu <Kit@dhcp-a0-99-9b-1d-ba-57.ubcvisitor.wifi.ubc.ca>
2017-01-22 13:29:59 -0800

    buttons look good now

---
+++ Assets/GetTurretCost.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetTurretCost : MonoBehaviour {
+	public MoneyManager Manager;
+	public Text TurretCostText;
+
+	// Use this for initialization
+	void Start () {
+		TurretCostText = gameObject.GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		TurretCostText.text = " Build \n Turret \n" + Manager.TurretCost + " Sand";
+
+	}
+}
